ffbd4dc13cf7a8ebadf4ffd61d11a0e189be7ef7
real.ilia.dzneladze@gmail.com <real.ilia.dzneladze@gmail.com>
2026-03-13 11:50:39 +0100

    Add Forward Pass, Gradients, and applied to Training

--- word2vec.py
+++ word2vec.py
@@ -77,9 +77,55 @@ def build_noise_distribuiton(word_counts, vocab_size):
     noise_dist = noise_dist / noise_dist.sum()
     return noise_dist
 
-### Stage 4: Initialize Embedding Vectors With Small Random Values
+
+### Stage 4: Initialize Embedding Vectors with Small Random Values
 
 def initialize_embeddings(vocab_size, embedding_dim):
     U = np.random.randn(vocab_size, embedding_dim) * 0.01
     V = np.random.randn(vocab_size, embedding_dim) * 0.01
     return U, V
+
+
+### Stage 5: Forward Pass and Loss
+
+# Normalize neg-pos to 0-1
+def sigmoid(x):
+    x = np.clip(x, -10, 10)
+    return 1.0 / (1.0 + np.exp(-x))
+
+# J = -log(pos_score) - sum(log(1 - neg_scores))
+def compute_loss(pos_score, neg_scores):
+    loss = -np.log(pos_score + 1e-10) - np.sum(np.log(1 - neg_scores + 1e-10))
+    return loss
+
+## Stage 6: Training: Gradients and Parameter Updates (The real meat of the script)
+
+def train_pair(w_c, w_o, neg_indices, U, V, lr):
+    # Forward Pass
+    u_c = U[w_c]
+    v_pos = V[w_o]
+    v_negs = V[neg_indices]
+
+    # Compute Dot Products
+    pos_dot = v_pos @ u_c
+    neg_dots = v_negs @ u_c
+
+    # Pass Through Sigmoid
+    pos_score = sigmoid(pos_dot)
+    neg_scores = sigmoid(neg_dots)
+
+    # Compute Loss
+    loss = compute_loss(pos_score, neg_scores)
+
+    # Compute Gradients
+    old_u_c = u_c.copy()
+    grad_v_pos = (pos_score - 1) * old_u_c
+    grad_v_negs = neg_scores.reshape(-1, 1) * old_u_c
+    grad_u = (pos_score - 1) * v_pos + (neg_scores.reshape(-1, 1) * v_negs).sum(axis=0)
+
+    # Apply updates (theta <- sum_i(sigma_i * v_wi))
+    U[w_c]        -= lr * grad_u
+    V[w_o]         -= lr * grad_v_pos
+    V[neg_indices] -= lr * grad_v_negs
+
+    return loss
